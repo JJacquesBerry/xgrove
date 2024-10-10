1a41ad4ac0eae6c1210b3bd8c7125754c7129cc2
JJacquesBerry <j.jacques.berry@gmail.com>
2024-10-10 11:39:46 +0200

    updated setup.py and added complete README

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 import codecs
 import os
 
 VERSION = '0.1.0'
 DESCRIPTION = 'creating and calculating groves of tree models'
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 # Setting up
 setup(
@@ -11,7 +14,8 @@ setup(
     version=VERSION,
     author="Jean Jacques Berry",
     author_email="<J.Jacques.Berry@gmail.com>",
-    description=DESCRIPTION,
+    description=DESCRIPTION,long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[],
     keywords=['tree', 'model', 'grove', 'forest'],
