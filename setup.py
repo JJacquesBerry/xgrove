8669194f507d9ac9ab88bf2ac2fb3bce8d5c9031
JJacquesBerry <j.jacques.berry@gmail.com>
2024-10-14 11:41:39 +0200

    updated Setup.py requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import codecs
 import os
 
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 DESCRIPTION = 'creating and calculating groves of tree models'
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text(encoding='utf-8')
@@ -17,7 +17,17 @@ setup(
     description=DESCRIPTION,long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "mkdocs",  # Für Dokumentation, falls erforderlich
+        "scikit-learn>=0.24.1",
+        "numpy>=1.19.2",
+        "matplotlib>=3.3.2",
+        "pandas>=1.1.3",
+        "graphviz>=0.16",
+        "statistics",  # Python eingebautes Modul, kein externes Package
+        # Überprüfen, ob eventuell zusätzliche Module benötigt werden, die hier nicht explizit genannt wurden
+    ],
+    python_requires='>=3.6',
     keywords=['tree', 'model', 'grove', 'forest'],
     classifiers=[
         "Development Status :: 1 - Planning",
